1c58cfe7ea4d3bcf9a96a74a4349a3f378033e50
JakubB <135221834+jakubB6@users.noreply.github.com>
2025-02-15 15:59:01 +0100

    Added Text Noise | Changed Hero Media

--- components/TextNoise.tsx
+++ components/TextNoise.tsx
@@ -13,16 +13,13 @@ const TextNoise = ({
     <span
       className={cn(
         "bg-background mt-3 relative rounded-md w-fit inline-block px-1.5 py-0.5 border",
-        textClassName
+        "before:absolute before:top-0 before:left-0 before:w-full before:h-full",
+        "before:content-[''] before:opacity-[0.1] before:z-10 before:pointer-events-none",
+        "before:bg-[url('/noise.gif')]",
+        overlayClassName
       )}
     >
-      {text}
-      <span
-        className={cn(
-          "absolute top-0 left-0 w-full h-full z-10 pointer-events-none bg-[url('/noise.gif')] opacity-[0.08]",
-          overlayClassName
-        )}
-      />
+      <span className={cn("text-foreground", textClassName)}>{text}</span>
     </span>
   );
 };
