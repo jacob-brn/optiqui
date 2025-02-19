86dedfd720864f3270be7f0eb39aafc82c7e38f9
JakubB <135221834+jakubB6@users.noreply.github.com>
2025-02-19 10:01:14 +0100

    Added new component: Rays

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFound() {
       <Link href="/" className={buttonVariants({})}>
         Back to homepage
       </Link>
-      <div className="absolute h-full w-full bg-[radial-gradient(hsl(var(--primary))_0.1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)] z-[-2]"></div>
+      <div className="-z-10 w-full h-full absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_30%_30%_at_50%_50%,#000_70%,transparent_120%)]"></div>
     </div>
   );
 }
