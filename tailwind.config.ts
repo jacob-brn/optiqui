db8b00bb00bde2394c64d6e269f2e287c224527c
JakubB <135221834+jakubB6@users.noreply.github.com>
2025-02-26 18:34:20 +0100

    Fixed Bento Grid Bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "./content/**/*.mdx",
+    "./contents/**/*.mdx",
     "./registry/**/*.{ts,tsx}",
   ],
   prefix: "",
